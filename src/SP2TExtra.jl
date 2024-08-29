91dbf49b73a69fb0b7ca00097a75b17a871fe6e7
Lance (Weiqing) Xu <lance.xwq@gmail.com>
2024-08-29 01:31:15 -0700

    add functions that handle trackmate xmls

--- src/SP2TExtra.jl
+++ src/SP2TExtra.jl
@@ -2,19 +2,19 @@ module SP2TExtra
 
 using GLMakie, SP2T, ColorSchemes, Combinatorics, StatsBase
 using NaNStatistics: histcounts, histcounts!
+using XMLDict
 
 export readbin, getframes, extractROI
-
 export getdarkcounts
-
 export ntracks, findMAP, findML, uncertainty1D, uncertainty2D
-
 export viewframes, visualize
+export xml2tracks
 
 include("import.jl")
 include("preprocess.jl")
 include("previsualize.jl")
 include("postprocess.jl")
 include("postvisualize.jl")
+include("trackmate.jl")
 
 end
---
+++ src/trackmate.jl
@@ -0,0 +1,34 @@
+function readparticle!(x::AbstractArray{<:Real,3}, particle::XMLDict.XMLDictElement, B::Integer, H::Integer)
+    for detection in particle["detection"]
+        trange = tryparse(Int, detection[:t]) * B .+ (1:B)
+        x[trange, 1, 1] .= tryparse(Float64, detection[:x]) + 0.5
+        x[trange, 2, 1] .= H - 0.5 - tryparse(Float64, detection[:y])
+    end
+end
+
+function readparticles!(x::AbstractArray{<:Real,3}, dict::XMLDict.XMLDictElement, B::Integer, H::Integer)
+    if dict["particle"] isa Vector
+        for (m, particle) in enumerate(dict["particle"])
+            readparticle!(view(x, :, :, m), particle, B, H)
+        end
+    else
+        readparticle!(view(x, :, :, 1), dict["particle"], B, H)
+    end
+end
+
+function xml2tracks(xml::String; batchsize::Integer, nframes::Integer, height::Integer, pxsize::Real)
+    xmlcontent = parse_xml(String(read(xml)))
+    ntracks = tryparse(Int, xmlcontent[:nTracks])
+    tracks = Array{Float64}(undef, nframes, 3, ntracks)
+    fill!(tracks, NaN)
+    tracks[:, 3, :] .= 0
+    readparticles!(tracks, xmlcontent, batchsize, height)
+    tracks .*= pxsize
+end
+
+function xml2tracks(xmls::AbstractVector{String}; batchsizes::AbstractVector{<:Integer}, nframes::Integer, height::Integer, pxsize::Real)
+    tracks = Vector{Vector{Float64}}()
+    for (xml, batchsize) in zip(xmls, batchsizes)
+        push!(tracks, xml2tracks(xml; batchsize=batchsize, nframes=nframes, height=height, pxsize=pxsize))
+    end
+end
