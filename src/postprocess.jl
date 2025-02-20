bf112e51c93d47bc955126695e01e75f0d705fe5
Lance (Weiqing) Xu <lance.xwq@gmail.com>
2025-02-20 11:18:11 -0700

    bug fix in credible2D

--- src/postprocess.jl
+++ src/postprocess.jl
@@ -6,15 +6,15 @@ function ntracks(chain::Chain; burn_in::Real=0)
     n
 end
 
-function credible1D(S::AbstractVector{<:Sample}, i::Integer, xrange::AbstractRange, yrange::AbstractRange; factor::Real=1, xshift::Real=0, yshift::Real=0)
+function credible1D(S::AbstractVector{<:Sample}, i::Integer, xedges::AbstractRange, yedges::AbstractRange; factor::Real=1, xshift::Real=0, yshift::Real=0)
     N = size(S[1].tracks, 1)
-    xcounts = zeros(Float64, N, length(xrange) - 1)
-    ycounts = zeros(Float64, N, length(yrange) - 1)
+    xcounts = zeros(Float64, N, length(xedges) - 1)
+    ycounts = zeros(Float64, N, length(yedges) - 1)
     ntracks = length(S)
     for s in S
         @views for (n, x) in enumerate(eachslice(s.tracks, dims=1))
-            histcounts!(xcounts[n, :], (x[1, i:i] .+ xshift) .* factor, xrange)
-            histcounts!(ycounts[n, :], (x[2, i:i] .+ yshift) .* factor, yrange)
+            histcounts!(xcounts[n, :], (x[1, i:i] .+ xshift) .* factor, xedges)
+            histcounts!(ycounts[n, :], (x[2, i:i] .+ yshift) .* factor, yedges)
         end
     end
     xcounts ./= ntracks
@@ -22,16 +22,16 @@ function credible1D(S::AbstractVector{<:Sample}, i::Integer, xrange::AbstractRan
     xcounts, ycounts
 end
 
-function credible1D(S::AbstractVector{<:Sample}, xrange::AbstractRange, yrange::AbstractRange; factor::Real=1, xshift::Real=0, yshift::Real=0)
+function credible1D(S::AbstractVector{<:Sample}, xedges::AbstractRange, yedges::AbstractRange; factor::Real=1, xshift::Real=0, yshift::Real=0)
     N = size(S[1].tracks, 1)
-    xcounts = zeros(Float64, N, length(xrange) - 1)
-    ycounts = zeros(Float64, N, length(yrange) - 1)
+    xcounts = zeros(Float64, N, length(xedges) - 1)
+    ycounts = zeros(Float64, N, length(yedges) - 1)
     ntracks = 0
     for s in S
         ntracks += size(s.tracks, 3)
         @views for (n, x) in enumerate(eachslice(s.tracks, dims=1))
-            histcounts!(xcounts[n, :], (x[1, :] .+ xshift) .* factor, xrange)
-            histcounts!(ycounts[n, :], (x[2, :] .+ yshift) .* factor, yrange)
+            histcounts!(xcounts[n, :], (x[1, :] .+ xshift) .* factor, xedges)
+            histcounts!(ycounts[n, :], (x[2, :] .+ yshift) .* factor, yedges)
         end
     end
     xcounts ./= ntracks
@@ -39,26 +39,26 @@ function credible1D(S::AbstractVector{<:Sample}, xrange::AbstractRange, yrange::
     xcounts, ycounts
 end
 
-function credible2D(S::AbstractVector{<:Sample}, i::Integer, xrange::AbstractRange, yrange::AbstractRange; factor::Real=1, xshift::Real=0, yshift::Real=0)
+function credible2D(S::AbstractVector{<:Sample}, i::Integer, xedges::AbstractRange, yedges::AbstractRange; factor::Real=1, xshift::Real=0, yshift::Real=0)
     N = size(S[1].tracks, 1)
-    counts = zeros(Float64, N, length(xrange) - 1, length(xrange) - 1)
+    counts = zeros(Float64, N, length(xedges) - 1, length(yedges) - 1)
     ntracks = length(S)
     for s in S
         @views for (n, x) in enumerate(eachslice(s.tracks, dims=1))
-            histcounts!(counts[n, :, :], (x[1, i:i] .+ xshift) .* factor, (x[2, i:i] .+ yshift .* factor), xrange, yrange)
+            histcounts!(counts[n, :, :], (x[2, i:i] .+ xshift) .* factor, (x[1, i:i] .+ yshift .* factor), yedges, xedges)
         end
     end
     counts ./= ntracks
 end
 
-function credible2D(S::AbstractVector{<:Sample}, xrange::AbstractRange, yrange::AbstractRange; factor::Real=1, xshift::Real=0, yshift::Real=0)
+function credible2D(S::AbstractVector{<:Sample}, xedges::AbstractRange, yedges::AbstractRange; factor::Real=1, xshift::Real=0, yshift::Real=0)
     N = size(S[1].tracks, 1)
-    counts = zeros(Float64, N, length(xrange) - 1, length(xrange) - 1)
+    counts = zeros(Float64, N, length(xedges) - 1, length(yedges) - 1)
     ntracks = 0
     for s in S
         ntracks += size(s.tracks, 3)
         @views for (n, x) in enumerate(eachslice(s.tracks, dims=1))
-            histcounts!(counts[n, :, :], (x[1, :] .+ xshift) .* factor, (x[2, :] .+ yshift) .* factor, xrange, yrange)
+            histcounts!(counts[n, :, :], (x[2, :] .+ xshift) .* factor, (x[1, :] .+ yshift) .* factor, yedges, xedges)
         end
     end
     counts ./= ntracks
