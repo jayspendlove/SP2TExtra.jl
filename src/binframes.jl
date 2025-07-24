5156ddcd600a38e53e40a0ef203507c9f2a21502
Lance (Weiqing) Xu <lance.xwq@gmail.com>
2025-07-24 01:26:42 -0700

    add functions for binning tracks

--- src/binframes.jl
+++ src/binframes.jl
@@ -1,8 +1,8 @@
-function binframes!(binned::AbstractArray{<:Integer,3}, frames1bit::AbstractArray{<:Integer,3}, batchsize::Integer)
+function sumbin!(binned::AbstractArray{T,3}, tobin::AbstractArray{T,3}, batchsize::Integer) where {T<:Real}
     @views for i in axes(binned, 3)
         sum!(
             binned[:, :, i],
-            frames1bit[:, :, (i-1)*batchsize+1:i*batchsize],
+            tobin[:, :, (i-1)*batchsize+1:i*batchsize],
         )
     end
     return binned
@@ -15,6 +15,29 @@ function binframes(frames1bit::AbstractArray{<:Integer,3}, batchsize::Integer)
         size(frames1bit, 2),
         size(frames1bit, 3) ÷ batchsize,
     )
-    binframes!(binned, frames1bit, batchsize)
+    sumbin!(binned, frames1bit, batchsize)
+    return binned
+end
+
+function meanbin!(binned::AbstractArray{T,3}, tobin::AbstractArray{T,3}, batchsize::Integer) where {T<:AbstractFloat}
+    @views for i in axes(binned, 3)
+        mean!(
+            binned[:, :, i],
+            tobin[:, :, (i-1)*batchsize+1:i*batchsize],
+            weights(ones(T, size(tobin, 1))),
+            dims=1,
+        )
+    end
+    return binned
+end
+
+function bintracks(tracks1bit::AbstractArray{<:Integer,3}, batchsize::Integer)
+    binned = similar(
+        tracks1bit,
+        size(tracks1bit, 1),
+        size(tracks1bit, 2),
+        size(tracks1bit, 3) ÷ batchsize,
+    )
+    meanbin!(binned, tracks1bit, batchsize)
     return binned
 end
