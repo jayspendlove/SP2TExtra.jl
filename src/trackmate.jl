6b0b5f751490ba46d7448e6e095fcd3be546a380
lanceXwq <lance.xwq@gmail.com>
2025-08-12 10:32:36 -0700

    no longer filp y-tracks when parsing

--- src/trackmate.jl
+++ src/trackmate.jl
@@ -1,35 +1,43 @@
-function readparticle!(x::AbstractArray{<:Real,2}, particle::XMLDict.XMLDictElement, B::Integer, H::Integer)
+# function readparticle!(x::AbstractArray{<:Real,2}, particle::XMLDict.XMLDictElement, B::Integer, H::Integer)
+#     for detection in particle["detection"]
+#         trange = tryparse(Int, detection[:t]) * B .+ (1:B)
+#         x[trange, 1] .= tryparse(Float64, detection[:x]) + 0.5
+#         x[trange, 2] .= H - 0.5 - tryparse(Float64, detection[:y])
+#     end
+# end
+
+function parsedetection!(x::AbstractArray{<:Real,2}, particle::XMLDict.XMLDictElement, batchsize::Integer)
     for detection in particle["detection"]
-        trange = tryparse(Int, detection[:t]) * B .+ (1:B)
-        x[trange, 1] .= tryparse(Float64, detection[:x]) + 0.5
-        x[trange, 2] .= H - 0.5 - tryparse(Float64, detection[:y])
+        trange = tryparse(Int, detection[:t]) * batchsize .+ (1:batchsize)
+        for (dim, coord) in enumerate([:x, :y, :z])
+            x[trange, dim] .= tryparse(Float64, detection[coord])
+        end
     end
 end
 
-function readparticles!(x::AbstractArray{<:Real,3}, dict::XMLDict.XMLDictElement, B::Integer, H::Integer)
+function parseparticle!(x::AbstractArray{<:Real,3}, dict::XMLDict.XMLDictElement, batchsize::Integer)
     if dict["particle"] isa Vector
-        for (m, particle) in enumerate(dict["particle"])
-            readparticle!(view(x, :, :, m), particle, B, H)
+        for (m, detection) in enumerate(dict["particle"])
+            parsedetection!(view(x, :, :, m), detection, batchsize)
         end
     else
-        readparticle!(view(x, :, :, 1), dict["particle"], B, H)
+        parsedetection!(view(x, :, :, 1), dict["particle"], batchsize)
     end
 end
 
-function xml2tracks(xml::String; batchsize::Integer, nframes::Integer, height::Integer, pxsize::Real)
+function xml2tracks(xml::String; batchsize::Integer=1, nframes::Integer, pxsize::Real=1)
     xmlcontent = parse_xml(String(read(xml)))
     ntracks = tryparse(Int, xmlcontent[:nTracks])
-    tracks = Array{Float64}(undef, nframes, 3, ntracks)
-    fill!(tracks, NaN)
-    tracks[:, 3, :] .= 0
-    readparticles!(tracks, xmlcontent, batchsize, height)
+    tracks = fill!(Array{Float64}(undef, nframes, 3, ntracks), NaN)
+    parseparticle!(tracks, xmlcontent, batchsize)
+    tracks .+= 0.5
     tracks .*= pxsize
 end
 
-function xml2tracks(xmls::AbstractVector{String}; batchsizes::AbstractVector{<:Integer}, nframes::Integer, height::Integer, pxsize::Real)
+function xml2tracks(xmls::AbstractVector{String}; batchsizes::AbstractVector{<:Integer}, nframes::Integer, pxsize::Real=1)
     tracks = Vector{Array{Float64,3}}()
     for (xml, batchsize) in zip(xmls, batchsizes)
-        push!(tracks, xml2tracks(xml; batchsize=batchsize, nframes=nframes, height=height, pxsize=pxsize))
+        push!(tracks, xml2tracks(xml; batchsize=batchsize, nframes=nframes, pxsize=pxsize))
     end
     tracks
 end
