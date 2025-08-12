8ccd59a7f914dc1f857faf847f7956194ae7520d
Lance (Weiqing) Xu <lance.xwq@gmail.com>
2025-08-12 02:07:23 -0700

    move visualization to SP2TVis

--- src/SP2TExtra.jl
+++ src/SP2TExtra.jl
@@ -1,20 +1,18 @@
 module SP2TExtra
 
-using GLMakie, SP2T, Combinatorics, StatsBase, TiffImages, MAT, ImageCore
+using  SP2T, Combinatorics, StatsBase, TiffImages, MAT, ImageCore
 using XMLDict
 
 export binframes, bintracks
 export readbin, getframes, extractROI, readtiff, writetiff, ImageJTIff
 export getdarkcounts
 export ntracks, credible1D, credible2D
-export viewframes
 export xml2tracks
 
 include("histcounts.jl")
 include("binframes.jl")
 include("import.jl")
 include("preprocess.jl")
-include("previsualize.jl")
 include("postprocess.jl")
 include("trackmate.jl")
 include("tiff.jl")
